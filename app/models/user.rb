24b9f48db8e8a796beb9d1d2b5a1d27faa1aface
Sébastien CEBULA <sebastien.cebula@gmail.com>
2018-06-17 17:01:50 +0200

    Completed authentication with virtual attrs for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,14 @@ class User < ApplicationRecord
   #confirmable -> The user has to confirm he's a human with a link in a mail.
   #lockable -> We can lock an user if he, for ex, fail login x times.
   #timeoutable -> We can log out an user after x hours/days ...
+
+  validates_presence_of :name
+
+  def first_name
+    self.name.split.first
+  end
+
+  def last_name
+    self.name.split.last
+  end
 end
